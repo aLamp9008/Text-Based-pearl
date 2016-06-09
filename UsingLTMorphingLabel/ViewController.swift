db8c12a46a7ae3112e4d90edab67f627c9a68dce
Alden Lamp <alamp9008@gmail.com>
2016-06-08 23:24:09 -0400

    Initial Commit

---
+++ UsingLTMorphingLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingLTMorphingLabel
+//
+//  Created by alden lamp on 6/8/16.
+//  Copyright © 2016 alden lamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
