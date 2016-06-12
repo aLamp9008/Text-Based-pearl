885de9e25eee42d6b880ffb2de50b446e9d1f44f
Alden Lamp <alamp9008@gmail.com>
2016-06-11 22:56:56 -0400

    condensing the first slide

--- UsingLTMorphingLabel/FirstSlide.swift
+++ UsingLTMorphingLabel/FirstSlide.swift
@@ -18,37 +18,7 @@ class FirstSlide : UIViewController {
     let font = UIFont(name: "Avenir", size: 21)
     let color = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1.00)
     var distance = 35
-    func makeLabel(whichLabel: Int) -> UILabel{
-        let distance = 5 + (30 * (Double(whichLabel) - 0.8))
-        let rect = CGRectMake(5, CGFloat(distance), self.view.frame.width - 10, 100)
-        let label = UILabel(frame: rect)
-        label.textColor = color
-        label.textAlignment = .Center
-        label.alpha = 1.0
-        label.font = font
-        switch whichLabel {
-        case 0:
-            label.text = "You wake up with light near the door"
-        case 1:
-            label.text = "and darkness on the other side of"
-        case 2:
-            label.text = "your house. As you go through the"
-        case 3:
-            label.text = "normal morning routine you your"
-        case 4:
-            label.text = "wife, Juana, sees a scorpion near"
-        case 5:
-            label.text = "Coyotito, your baby. In an attempt"
-        case 6:
-            label.text = "to save the baby, Coyotito gets stung"
-        case 7:
-            label.text = "you are left with two options."
-        default:
-            break
-        }
-        label.fitFontForSize(10, maxFontSize: font!.pointSize, accuracy: 1.0)
-        return label
-    }
+
     
     @IBOutlet weak var option1: UIButton!
     
@@ -56,69 +26,54 @@ class FirstSlide : UIViewController {
     
     
     
-    
+    var instructionLabels = [UILabel]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(view.subviews)
-        let label0 = makeLabel(0)
-        let label1 = makeLabel(1)
-        let label2 = makeLabel(2)
-        let label3 = makeLabel(3)
-        let label4 = makeLabel(4)
-        let label5 = makeLabel(5)
-        let label6 = makeLabel(6)
-        let label7 = makeLabel(7)
-        
-        delay(1.0){
-            self.view.addSubview(label0)
-            label0.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-            self.delay(1.0, closure: {
-                self.view.addSubview(label1)
-                label1.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-                self.delay(1.0, closure: {
-                    self.view.addSubview(label2)
-                    label2.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-                    self.delay(1.0, closure: {
-                        self.view.addSubview(label3)
-                        label3.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-                        self.delay(1.0, closure: {
-                            self.view.addSubview(label4)
-                            label4.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-                            self.delay(1.0, closure: {
-                                self.view.addSubview(label5)
-                                label5.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-                                self.delay(1.0, closure: {
-                                    self.view.addSubview(label6)
-                                    label6.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-                                    self.delay(1.0, closure: {
-                                        self.view.addSubview(label7)
-                                        label7.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
-                                        self.delay(1.0, closure: {
-                                            self.option1.setTitle("go to the doctor for help", forState: .Normal)
-                                            self.option2.setTitle("rely on Juana curing him", forState: .Normal)
-                                        })
-                                    })
-                                })
-                            })
-                        })
-                    })
-                })
+        for i in 0...7{
+            let distance = 5 + (30 * (Double(i) - 0.8))
+            let rect = CGRectMake(5, CGFloat(distance), self.view.frame.width - 10, 100)
+            let label = UILabel(frame: rect)
+            label.textColor = color
+            label.textAlignment = .Center
+            label.alpha = 1.0
+            label.font = font
+            switch i {
+            case 0:
+                label.text = "You wake up with light near the door"
+            case 1:
+                label.text = "and darkness on the other side of"
+            case 2:
+                label.text = "your house. As you go through the"
+            case 3:
+                label.text = "normal morning routine you your"
+            case 4:
+                label.text = "wife, Juana, sees a scorpion near"
+            case 5:
+                label.text = "Coyotito, your baby. In an attempt"
+            case 6:
+                label.text = "to save the baby, Coyotito gets stung"
+            case 7:
+                label.text = "you are left with two options."
+            default:
+                break
+            }
+            label.fitFontForSize(10, maxFontSize: font!.pointSize, accuracy: 1.0)
+            instructionLabels.append(label)
+            delay(Double(i) * 1.0 + 1.0, closure: {
+                self.view.addSubview(label)
+                label.drawOutlineAnimatedWithLineWidth(1.0, withDuration: 1, fadeToLabel: true)
             })
-
-
-
-
-
-
-
-
-            
-
-            
         }
+        
+        delay(9.0) { 
+            self.option1.setTitle("go to the doctor for help", forState: .Normal)
+            self.option2.setTitle("rely on Juana curing him", forState: .Normal)
+        }
+        
     }
     
     
